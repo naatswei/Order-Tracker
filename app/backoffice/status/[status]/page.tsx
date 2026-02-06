27ce9e4847f130ba868e8b30a8f69dc1f81f7603
naatswei <naadromoangelaadjei@gmail.com>
2026-02-06 09:30:49 +0000

    Remove background color from Package icon in backoffice headers

--- app/backoffice/status/[status]/page.tsx
+++ app/backoffice/status/[status]/page.tsx
@@ -58,7 +58,7 @@ export default function StatusFilterPage() {
                 <div className="w-full px-[30px] py-4">
                     <div className="flex items-center justify-between">
                         <div className="flex items-center gap-3">
-                            <div className="w-10 h-10 bg-[#191A43]/10 rounded-xl flex items-center justify-center shrink-0">
+                            <div className="w-10 h-10 rounded-xl flex items-center justify-center shrink-0">
                                 <Package className="text-[#191A43] w-5 h-5" />
                             </div>
                             <div className="hidden sm:block">
